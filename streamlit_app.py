8095bfb18c5899379253ac3afd7a8ebab2843cf3
MahammadImranShaik <140884759+MahammadImranShaik@users.noreply.github.com>
2023-07-29 15:05:00 +0530

    Update streamlit_app.py
    Added header and menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Yogurt')
+streamlit.text('Oat Meal')
+streamlit.text('Strawrberry')
